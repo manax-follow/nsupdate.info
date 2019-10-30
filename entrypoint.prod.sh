6536568720b1669df9477b1604b03645158e36ba
Bruno  Santeramo <bruno.santeramo@gmail.com>
2019-10-31 00:37:22 +0100

    wip production environemnt

---
+++ entrypoint.prod.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# python manage.py makemigrations
+# python manage.py flush --no-input
+# python manage.py migrate
+
+# RUN python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+
+
+exec "$@"
